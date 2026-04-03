f815c881848b6fef188f8db85b94ffe524d3ab1b
4DA <4da@sandycat.info>
2026-04-04 00:04:18 +0500

    [cleanup] remove resolve_rig_path

--- src/sv_app/src/sources/file_sequence_source.cpp
+++ src/sv_app/src/sources/file_sequence_source.cpp
@@ -1,23 +1,7 @@
 #include "sources/file_sequence_source.hpp"
 
-#include <fstream>
-
 #include "sources/render_bridge_4cam.hpp"
 #include "sources/rigged_png_source_support.hpp"
-
-namespace
-{
-
-std::string resolve_rig_path(const std::string &rig_path)
-{
-    if (std::ifstream(rig_path).good()) {
-        return rig_path;
-    }
-
-    return rig_path;
-}
-
-} // namespace
 
 namespace svapp
 {
@@ -36,9 +20,8 @@ FileSequenceSource::FileSequenceSource(FileSequenceSourceConfig config):
 
 bool FileSequenceSource::open()
 {
-    const std::string rig_path = resolve_rig_path(_config.rig_path);
     return open_rigged_png_source_for_render_bridge_4cam(
-        rig_path,
+        _config.rig_path,
         _config.frame_paths,
         _rig,
         _info,
